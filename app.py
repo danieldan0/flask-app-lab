66db9be57d7b704da0336fceef9a8b20d66f4bd1
Daniel <daniel.nakushnov@gmail.com>
2024-10-16 17:30:49 +0300

    Added templates

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
-from flask import Flask, request, redirect, url_for
-app = Flask(__name__)
-app.config.from_pyfile('config.py')
+from flask import Flask, request, redirect, url_for, render_template
 
-@app.route('/')   # URL '/' to be handled by main() route handler
+app = Flask(__name__)
+app.config.from_pyfile("config.py")
+
+@app.route('/')
 def main():
-    return 'Hello, world!'
+    return render_template("hello.html")
 
 @app.route('/homepage') 
 def home():
     """View for the Home page of your website."""
     agent = request.user_agent
-    return f"<h1>This is your homepage :) - {agent}</h1> "
 
-@app.route('/hi/<string:name>')  # /hi/ivan?age=30
+    return render_template("home.html", agent=agent)
+
+@app.route("/hi/<string:name>")   #/hi/ivan?age=45&q=fdfdf
 def greetings(name):
     name = name.upper()
-    age = request.args.get("age", 0, type=int)
+    age = request.args.get("age", 0, int)
+    year = 2024 - age
 
-    return f"Welcome {name=} {age=}"
+    return f"Welcome, {name}  {year}"
 
-@app.route('/admin')
+@app.route("/admin")
 def admin():
-    to_url = url_for("greetings", name="administrator", _external=True)               # --> "/hi/admin"
+    to_url = url_for("greetings", name="administrator", age=45, _external=True)     # "http://localhost:8080/hi/administrator?age=45"
     print(to_url)
     return redirect(to_url)
 
+
 if __name__ == "__main__":
     app.run()  # Launch built-in web server and run this Flask webapp, debug=True
+ 
+

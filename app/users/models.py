11f5a322b1defd14353628b4424fb223cd7126ef
Daniel <daniel.nakushnov@gmail.com>
2024-12-18 11:24:59 +0200

    Added image file to users

--- app/users/models.py
+++ app/users/models.py
@@ -8,6 +8,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    image_file = db.Column(db.String(20), nullable=True, default="default.png")
 
     def __repr__(self):
         return f"User ('{self.email}')"
